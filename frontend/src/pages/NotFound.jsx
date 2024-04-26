2916b50943dce489ada2328fc64a349421b45a52
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2024-04-25 22:43:47 -0300

    feat: :sparkles: Making a Generic Form
    Começando a construir um formulário básico para o prjeto

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable react/no-unescaped-entities */
 function NotFound() {
     return <div>
         <h1>404 Not Found</h1>
-        <p>The page you're looking for does not exist!😢</p>
+        <p>The page you are looking for doesn't exist!😢</p>
     </div>
 }
 
